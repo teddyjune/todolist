d377c5f96ba5c83d5467efe2c37ccc31798e6602
yeonjoo.lee <teddyjune@naver.com>
2022-12-29 19:58:53 +0900

    feat: Hive 사용준비

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:todolist/list_screen.dart';
+import 'package:todolist/todo.dart';
 
-void main() {
+//어디에서나 쓸 수 있다. 초보라서 어디서든 가져다쓰기 편하게 해 놓은 것.
+late final Box<Todo> todos;
+
+void main() async {
+  //hive 초기화 코드
+  await Hive.initFlutter();
+  Hive.registerAdapter(TodoAdapter());
+  Box<Todo> todos = await Hive.openBox<Todo>('todolist.db');
   runApp(const MyApp());
 }
 
@@ -15,7 +24,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: ListScreen(),
+      home: const ListScreen(),
     );
   }
 }
--- lib/todo.dart
+++ lib/todo.dart
@@ -1,5 +1,12 @@
-class Todo {
+import 'package:hive/hive.dart';
+part 'todo.g.dart';
+@HiveType(typeId: 0)
+class Todo extends HiveObject {
+  @HiveField(0)
+  int? id;
+  @HiveField(1)
   String title;
+  @HiveField(2)
   int dateTime;
 
   Todo({
